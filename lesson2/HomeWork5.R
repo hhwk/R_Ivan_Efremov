b6b13e6d92aa7534187aa53ade33e18a369b87ea
hhwk <kripvan@gmail.com>
2023-11-09 20:48:18 +0300

    Add files via upload

---
+++ lesson2/HomeWork5.R
@@ -0,0 +1,18 @@
+library(dplyr)
+library(tidyr)
+library(stringr)
+library(ggplot2)
+setwd("C:/Users/koly36/Desktop/RScripts")
+
+
+df <- read.csv("ДЗ3_superstore_data.csv", header = TRUE)
+df <- df %>% mutate(Age = 2023 - Year_Birth, Rich_flag = Income > 80000)
+df_updated <- unite(df, col='Education и Marital_Status', c(Education, Marital_Status ), sep = ";", remove = TRUE)
+df_updated$Rich_flag = as.numeric(df_updated$Rich_flag)
+haveNanValues <- function(x) {
+  any(is.nan(x))
+}
+print(sapply(df_updated, haveNanValues))
+plot1 <- ggplot(df_updated, aes(x=factor(Response), y=Age)) + geom_boxplot()
+print(plot1)
+print(df_updated[str_which(as.factor(df_updated$Age), "^.*9.*$"),])
